f9890b023e9df8796b12210a4f5b38a67a90e02e
aweso <jennizaru11@gmail.com>
2024-01-23 13:51:07 -0800

    Create and defined GET, POST, PUT, DELETE routes/endpoints

--- src/main/java/edu/greenriver/dadjokes/controllers/DadJokeController.java
+++ src/main/java/edu/greenriver/dadjokes/controllers/DadJokeController.java
@@ -1,7 +1,10 @@
 package edu.greenriver.dadjokes.controllers;
 
+import edu.greenriver.dadjokes.domain.DadJoke;
 import edu.greenriver.dadjokes.services.DadJokeService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DadJokeController
@@ -11,5 +14,30 @@ public class DadJokeController
     public DadJokeController(DadJokeService service)
     {
         this.service = service;
+    }
+
+    @GetMapping("jokes")
+    public List<DadJoke> getAll()
+    {
+        return service.getAll();
+    }
+
+    @PostMapping("jokes")
+    public void addJoke(@RequestBody DadJoke newDadJoke)
+    {
+        service.add(newDadJoke);
+    }
+
+    @PutMapping("jokes/{id}")
+    public DadJoke updateDadJoke(@PathVariable int id,
+                                 @RequestBody String updatedDadJoke)
+    {
+        return service.update(id, updatedDadJoke);
+    }
+
+    @DeleteMapping("jokes/{id}")
+    public void deleteDadJoke(@PathVariable int id)
+    {
+        service.delete(id);
     }
 }
